e755f70376220ab84829cb011f276033f095b43c
luiCham <luis.chamat@loanlogics.com>
2021-09-24 16:48:35 -0500

    Reutilizando código del frontend

--- web/app.py
+++ web/app.py
@@ -5,20 +5,20 @@ app = Flask(__name__, template_folder='templates')
 
 variables_list = ['PH','Temperatura','Humedad Tierra','Humedad Aire','Distancia','Luz Solar']
 
-@app.route('/listarSensores',methods=['GET'])
+@app.route('/listar-agrocadenas',methods=['GET'])
 def listarSensores():
-    sensores_list = requests.get('http://localhost:5000/tipoSensores').json()
-    return render_template('listarSensores.html', sensores=sensores_list)
+    agrocadenas_list = requests.get('http://18.217.185.186:5000/agrocadenas').json()
+    return render_template('listarAgrocadenas.html', agrocadenas=agrocadenas_list)
     
-@app.route('/crearSensor',methods=['GET'])
+@app.route('/crear-agrocadena',methods=['GET'])
 def crearSensor():
     return render_template('crearSensor.html', variables=variables_list)
 
-@app.route('/guardarSensor',methods=['POST'])
+@app.route('/guardar-agrocadena',methods=['POST'])
 def guardarSensor():
     sensor = dict(request.values)
     sensor['precio'] = int(sensor['precio'])
-    requests.post('http://localhost:5000/tipoSensores',json=sensor)
+    requests.post('http://18.217.185.186:5000/agrocadena',json=sensor)
     return(listarSensores())
 
 app.run(host='0.0.0.0',port=8000)
