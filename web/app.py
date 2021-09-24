6adc171fd73fb12ad60d63adfa2994727ccb8c36
luiCham <luis.chamat@loanlogics.com>
2021-09-24 16:51:13 -0500

    Modifying names

--- web/app.py
+++ web/app.py
@@ -3,22 +3,20 @@ import requests
 
 app = Flask(__name__, template_folder='templates')
 
-variables_list = ['PH','Temperatura','Humedad Tierra','Humedad Aire','Distancia','Luz Solar']
-
 @app.route('/listar-agrocadenas',methods=['GET'])
-def listarSensores():
+def listarAgrocadenas():
     agrocadenas_list = requests.get('http://18.217.185.186:5000/agrocadenas').json()
     return render_template('listarAgrocadenas.html', agrocadenas=agrocadenas_list)
     
 @app.route('/crear-agrocadena',methods=['GET'])
-def crearSensor():
-    return render_template('crearSensor.html', variables=variables_list)
+def crearAgrocadena():
+    return render_template('crearAgrocadena.html')
 
 @app.route('/guardar-agrocadena',methods=['POST'])
 def guardarSensor():
     sensor = dict(request.values)
     sensor['precio'] = int(sensor['precio'])
     requests.post('http://18.217.185.186:5000/agrocadena',json=sensor)
-    return(listarSensores())
+    return(listarAgrocadenas())
 
 app.run(host='0.0.0.0',port=8000)
